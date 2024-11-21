b9a537bb18415bd0a7153de4324025e720708029
alkaz07 <alkaz07@yandex.ru>
2024-11-21 19:04:05 +0300

    get -> Post

--- src/main/java/com/example/wordStatDemo/WordStatDemoApplication.java
+++ src/main/java/com/example/wordStatDemo/WordStatDemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.io.IOException;
 
@@ -30,15 +32,21 @@ public class WordStatDemoApplication {
 		return "wordStat";
 	}
 
-	@GetMapping("/getresult")
-	public String getresult(String username, String word, Model model){
+	//@GetMapping("/getresult")
+	@PostMapping("/getresult")
+	public String getresult(String username, String word
+			, Model model
+	//							 , RedirectAttributes model
+	){
 		System.out.println("получено word = " + word);
 		int x = wordCheck.saveWord(username, word);
+		//String sx = String.valueOf(x);
 		model.addAttribute("x", x);
 		System.out.println("wordCheck = " + wordCheck.userWords);
 		System.out.println("пользователи, которые вводили это слово = " + wordCheck.getUsersByWord(word));
 		System.out.println("слова, которые вводил этот пользователь = " + wordCheck.getWordsByUser(username));
 		return "wordStat";
+		//return new RedirectView("/wordStat");
 	}
 
 	@GetMapping("/adminka")
